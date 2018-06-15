21a3782505d7f653bb25dfc7921d0c7159c5ea6d
Robert Marin <triplehalo@gmail.com>
2018-06-14 22:29:03 -0700

    Update navigation.js
    String instead of variable, ugh

--- vue/navigation.js
+++ vue/navigation.js
@@ -19,7 +19,7 @@ Vue.component('navigation',{
             <div class="collapse navbar-collapse" id="navbarSupportedContent">
                 <ul class="navbar-nav mr-auto container-row">
                     <li class="nav-item col" v-for="option in options">
-                        <a class="nav-link" :class="{active: this.selected == option}" href="#" @click="select('option')">{{option}}</a>
+                        <a class="nav-link" :class="{active: this.selected == option}" href="#" @click="select( option )">{{option}}</a>
                     </li>
                 </ul>
             </div>
