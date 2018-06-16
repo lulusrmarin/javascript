0c710d4e2b118173ca32235485e2663f0ee9b88b
Robert Marin <triplehalo@gmail.com>
2018-06-15 23:01:15 -0700

    Create pagination.js
    Uses  Bootstrap4 pagination combined with Vue.js component.  Currently the component only takes one prop, which is a number of total items.  There is internal data such as the current page, results per page default, available results per page, and the range (being how many pages should be padding before or after the current page in the element.  These would stand to be better  used as props so they can live individually on each instance of the component.

---
+++ vue/pagination.js
@@ -0,0 +1,68 @@
+Vue.component('pagination',{
+    props: [ 'total' ],
+    data: function() {
+        return {
+            currentPage: 1,
+            resultsPerPage:  25,
+            resultsOptions: [ 25, 50, 75, 100 ],
+            range: 3
+        }
+    },
+    computed: {
+        maxPage: function() {
+            return this.total / this.resultsPerPage
+        },
+        displaying: function() {
+            return "Displaying " + (this.currentPage - 1) * this.resultsPerPage + " - " +
+                parseInt( ( this.currentPage * this.resultsPerPage ) > this.total ? this.total : ( this.currentPage * this.resultsPerPage ) ) +
+                " of " + this.total + " videos";
+        }
+    },
+    methods: {
+        loadPage: function( pg ) {
+            this.currentPage = pg;
+        },
+        resetPage: function() {
+            this.currentPage = 1;
+        }
+    },
+    template: `
+        <div class="row">
+            <div class="col text-center">
+                <nav aria-label="Page navigation example" class="text-center container-fluid">
+                  <ul class="pagination container-fluid text-center">
+                    <li class="page-item"><a class="page-link" href="#" @click="loadPage( 1 )" v-if="currentPage > 1">First</a></li>                              
+                    <li class="page-item"><a class="page-link" href="#" @click="loadPage( currentPage - 1 )" v-if="currentPage > 1">Prev</a></li>
+
+                    <li class="page-item" v-for="i in range">
+                        <a class="page-link" href="#" @click="loadPage( currentPage - i )" v-if="currentPage > i">{{currentPage - i}}</a>
+                    </li>                             
+
+                    <li class="page-item"><a class="page-link">{{currentPage}}</a></li>
+
+                    <li class="page-item" v-for="i in range">
+                        <a class="page-link" href="#" @click="loadPage( currentPage + i )" v-if="currentPage < maxPage">{{currentPage + i}}</a>
+                    </li>
+
+                    <li class="page-item"><a class="page-link" href="#" @click="loadPage( currentPage + 1 )" v-if="currentPage < maxPage">Next</a></li>
+                    <li class="page-item"><a class="page-link" href="#" @click="loadPage( maxPage )" v-if="currentPage < maxPage">Last</a></li>
+                  </ul>
+                </nav>
+            </div>
+
+            <div class="col text-center">{{displaying}}</div>
+
+            <div class="col">
+                <div class="row">
+                    <div class="col align-bottom text-right">Results Per Page:</div>
+                    <div class="col">
+                        <select class="form-control" v-model="resultsPerPage" @change="resetPage()">
+                            <option v-for="option in resultsOptions">{{option}}</option>
+                        </select>        
+                    </div>
+                </div>
+            </div>
+            <search placeholder="Search For Videos"></search>
+        </div>                
+    `
+});
